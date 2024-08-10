090e87c8ba0543c944658a1ce5a8c925a1673d09
aliensmart <kaoua17@gmail.com>
2024-08-10 20:09:59 +0000

    added gradient_problem

---
+++ algorithm/dynamic-array/index.js
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/dynamic-array/problem?isFullScreen=true
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+  // Write your code here
+  let lastAnswer = 0;
+  let arr = [];
+  arr[0] = [];
+  arr[1] = [];
+  
+}
